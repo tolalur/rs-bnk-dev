4f06dd875560e02da483387efd5a6b572ddbf2b2
melnikovanadya <melnikova@ibsys.ru>
2021-10-08 02:08:44 +0300

    #9 форма общей информации в заявке

--- src/app/main/request/general/general.component.ts
+++ src/app/main/request/general/general.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-general',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./general.component.scss']
 })
 export class GeneralComponent implements OnInit {
+  @Input() isAdd: boolean | undefined;
 
+  generalForm = new FormGroup({
+    idProject: new FormControl(null ),
+    owner: new FormControl(null ),
+    administratorsGroup: new FormControl(null ),
+    mnemonicName: new FormControl(null ),
+    budgetLinks: new FormControl(null ),
+  });
   constructor() { }
 
   ngOnInit(): void {
+    console.log('isAdd: ', this.isAdd);
   }
 
 }
