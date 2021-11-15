6a930679bcc8a9a8ac7493e1e5e80b4d22559cd9
Tolkachev Alexsey <atolkachev@sportmaster.ru>
2021-11-15 22:25:33 +0300

    вывод данных в таблице заявок #2

--- src/app/main/types/request-list.model.ts
+++ src/app/main/types/request-list.model.ts
@@ -1,11 +1,11 @@
 export interface RequestListModel {
   id: string;
   status: string;
-  createdAt: "2021-11-09T17:43:56.922726"
-  projectNumber: "String"
-  reservedTo: null
-  respUserEmail: "user@email.com"
-  respUserFio: null
-  respUserId: 1
-  updatedAt: "2021-11-11T14:32:21.209211"
+  createdAt: string;
+  projectNumber: string;
+  reservedTo: string;
+  respUserEmail: string;
+  respUserFio: string
+  respUserId: number;
+  updatedAt:  string;
 }
