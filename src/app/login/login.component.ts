5643904cb28f064625e158b18e338d5131530e38
melnikovanadya <melnikova@ibsys.ru>
2021-11-18 16:54:33 +0300

    логин

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -37,7 +37,6 @@ export class LoginComponent implements OnInit, OnDestroy {
   }
 
   onLogin() {
-    console.log('pip');
     if (this.password.valid && this.login.valid) {
       this.userService
         .login({login: this.login.value, password: this.password.value})
